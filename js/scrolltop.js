05ab89d88cfff29977b96e6c1b181477e9742136
Jeffery <jefffery.li@gmail.com>
2017-10-31 11:17:56 -0400

    added scroll top js

---
+++ js/scrolltop.js
@@ -0,0 +1,16 @@
+// Scroll to top button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200)
+		document.getElementById("scrollTop").style.display = "block"
+	
+	else {
+		document.getElementById("scrollTop").style.display = "none"
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
