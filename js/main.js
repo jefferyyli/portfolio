f9db5e14658bc37c69c621ec791b697bd2c0877d
Jeffery <jefffery.li@gmail.com>
2017-11-01 14:11:42 -0400

    wip home

--- js/main.js
+++ js/main.js
@@ -1,16 +1,38 @@
 // Scroll to top button
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200)
-		document.getElementById("scrollTop").style.display = "block"
+// function scrollFunction() {
+// 	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200)
+// 		document.getElementById("scrollTop").style.display = "block"
 	
-	else {
-		document.getElementById("scrollTop").style.display = "none"
-	}
-}
+// 	else {
+// 		document.getElementById("scrollTop").style.display = "none"
+// 	}
+// }
 
 function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
+var timeOut;
+function scrollToTop() {
+	if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250){
+		window.scrollBy(0,-100);
+		timeOut=setTimeout('scrollToTop()',10);
+		document.getElementById("scrollToTop").style.display = "block";
+	}
+	else clearTimeout(timeOut);
+	document.getElementById("scrollToTop").style.display = "none";
+}
+
+// function scrollToTop() {
+// 	var scrollToTop = document.getElementById("scrollToTop");
+// 	    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+// 	        window.scrollBy(0, -200);
+// 	        requestAnimationFrame(scrollToTop);
+// 	        document.getElementById("scrollToTop").style.display = "block";
+// 	    } else {
+// 	    	document.getElementById("scrollToTop").style.display = "none";
+// 	    }
+// 	}
