fa34e2436c40fe56af94972bd589e2816dbeac8c
Jeffery <jefffery.li@gmail.com>
2017-11-07 14:28:46 -0500

    added analytics

--- js/scrolltop.js
+++ js/scrolltop.js
@@ -2,9 +2,9 @@
 var timeOut;
 function scrollToTop() {
 	if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
-		window.scrollBy(0,-10);
+		window.scrollBy(0,-200);
 		timeOut=setTimeout('scrollToTop()',10);
-		requestAnimationFrame(scrollToTop);
+		// requestAnimationFrame(scrollToTop);
 		document.getElementById("scrollToTop").style.display = "block";
 	}
 	else clearTimeout(timeOut);
